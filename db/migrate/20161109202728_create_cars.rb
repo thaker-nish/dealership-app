2853c58199348999e8d182b5de575158fd68104e
thaker-nish <thaker.nish@gmail.com>
2016-11-09 14:28:26 -0600

    Initial commit

---
+++ db/migrate/20161109202728_create_cars.rb
@@ -0,0 +1,12 @@
+class CreateCars < ActiveRecord::Migration[5.0]
+  def change
+    create_table :cars do |t|
+      t.string :make
+      t.string :model
+      t.string :image
+      t.string :price
+
+      t.timestamps
+    end
+  end
+end
